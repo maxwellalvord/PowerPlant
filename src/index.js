d13ea0a7839c6218094d12456933102d858d5a37
Maxwell Alvord <maxwellalvord@gmail.com>
2022-08-30 13:45:16 -0700

    add multiple state controls for multiple plants
    Co-authored-by: Ryan DeFea <ryan.defea@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 
+
 // This function stores our state.
 
 const storeState = () => {
@@ -11,8 +12,14 @@ const storeState = () => {
   }
 }
 
-const stateControl = storeState();
+// const plant = {
+//   soil: 0,
+//   water: 0,
+//   light: 0
+// }
 
+const stateControl = storeState();
+const stateControl2 = storeState();
 
 // This is a function factory. We can easily create more specific functions that alter a plant's soil, water, and light to varying degrees.
 
@@ -25,7 +32,7 @@ const changeState = (prop) => {
   }
 }
 
-const plantNew = {...state, water: 5};
+// const plantNew = {...state, water: 5};
 // We create four functions using our function factory. We could easily create many more.
 const giveLight = changeState("light")(5);
 
@@ -48,7 +55,6 @@ $(document).ready(function () {
     $('#current-soil-value').text(`Soil State: ${currentState.soil}`);
     $('#current-water-value').text(`Water State: ${currentState.water}`);
     $('#current-light-value').text(`Light State: ${currentState.light}`);
-
   });
 
   $('#feed').click(function () {
@@ -69,24 +75,33 @@ $(document).ready(function () {
 
 
 
-  $("#new-plant").click(function(){
-    var newPlant = {soil: 1, light: 3, water: -3}
-    const stateChangeFunction = storeState(newPlant);
-  });
+  // $("#new-plant").click(function(){
+  //   var newPlant = {soil: 1, light: 3, water: -3}
+  //   const stateChangeFunction = storeState(newPlant);
+  //   console.log(newPlant);
+  // });
 
   $('#feed1').click(function () {
-    const newPlant = stateControl(blueFood);
+    const newPlant = stateControl2(blueFood);
     $('#soil-value1').text(`Soil: ${newPlant.soil}`);
   });
 
   $('#water1').click(function () {
-    const newPlant = stateControl(superWater);
+    const newPlant = stateControl2(superWater);
     $('#water-value1').text(`Water: ${newPlant.water}`);
   });
 
   $('#light1').click(function () {
-    const newPlant = stateControl(giveLight);
+    const newPlant = stateControl2(giveLight);
     $('#light-value1').text(`Light: ${newPlant.light}`);
+  });
+
+  $('#show-state1').click(function () {
+    // We just need to call stateControl() without arguments to see our current state.
+    const newPlant = stateControl2();
+    $('#current-soil-value1').text(`Soil State: ${newPlant.soil}`);
+    $('#current-water-value1').text(`Water State: ${newPlant.water}`);
+    $('#current-light-value1').text(`Light State: ${newPlant.light}`);
   });
 });
 
